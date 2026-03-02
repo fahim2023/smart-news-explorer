2729777173ab20d4eb1428bb03a1283bc0402ab0
fahim2023 <fa733m4d4m@outlook.com>
2026-03-02 13:25:55 +0000

    add a basic fetch call function and call it upon dom content load

---
+++ assets/js/model.js
@@ -0,0 +1,25 @@
+const API_KEY = `3ed52fe5-f7cc-4d73-b9df-39d8cbff51f8`;
+const BASE_URL = `https://content.guardianapis.com/search`;
+const PAGE_SIZE = 12;
+
+/*
+the dom event listener will trigger as soon as dom is loaded
+ and fetch latest articles on all sections
+*/
+document.addEventListener("DOMContentLoaded", fetchLatestArticles);
+async function fetchLatestArticles() {
+  try {
+    const url = `${BASE_URL}?page-size=${PAGE_SIZE}&api-key=${API_KEY}`;
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Network response failed");
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching articles:", error);
+  }
+}
