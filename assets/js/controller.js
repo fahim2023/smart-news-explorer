361871b8ab6697a7c576c28949e6f8bc36956eec
fahim2023 <fa733m4d4m@outlook.com>
2026-03-05 07:01:43 +0000

    replace static query string with  URLSearchParams to dynamically append search query,section,date sorting filters and apikey, this will now allow the rendering of various sections based on what we select from the dropdown selection, eg education, technology,politics etc

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -11,18 +11,22 @@ const state = {
   total: 0,
   results: [],
 };
-document.addEventListener("DOMContentLoaded", async () => {
+async function loadArticles() {
   try {
-    const sectionSelect = document.getElementById("sectionSelect");
+    const articles = await model.fetchLatestArticles(state);
+    state.results = articles;
+    DefaultView.renderArticles(state.results);
+  } catch {}
+}
+document.addEventListener("DOMContentLoaded", async () => {
+  const sectionSelect = document.getElementById("sectionSelect");
 
-    sectionSelect.addEventListener("change", async (event) => {
-      state.section = event.target.value;
-      state.page = 1;
-    });
-    const articles = await model.fetchLatestArticles();
-    DefaultView.renderArticles(articles);
-  } catch (error) {
-    DefaultView.showError("Failed to load articles.");
-    console.error(error);
-  }
+  sectionSelect.addEventListener("change", async (event) => {
+    state.section = event.target.value;
+    state.page = 1;
+    console.log("Selected section:", state.section);
+
+    await loadArticles();
+  });
+  await loadArticles();
 });
--- assets/js/model.js
+++ assets/js/model.js
@@ -6,9 +6,22 @@ const PAGE_SIZE = 12;
 the dom event listener will trigger as soon as dom is loaded
  and fetch latest articles on all sections
 */
-export async function fetchLatestArticles() {
+export async function fetchLatestArticles(state) {
+  const params = new URLSearchParams();
+  if (state.query) params.append("q", state.query);
+  if (state.section && state.section !== "all")
+    params.append("section", state.section);
+  if (state.fromDate) params.append("from-date", state.fromDate);
+  if (state.toDate) params.append("to-date", state.toDate);
+
+  params.append("order-by", state.sort);
+  params.append("page", state.page);
+  params.append("page-size", state.pageSize);
+  params.append("api-key", API_KEY);
+  params.append("show-fields", "trailText,headline,thumbnail");
+
   try {
-    const url = `${BASE_URL}?page-size=${PAGE_SIZE}&order-by=newest&show-fields=thumbnail,trailText,headline&api-key=${API_KEY}`;
+    const url = `${BASE_URL}?${params.toString()}`;
     const response = await fetch(url);
 
     if (!response.ok) {
@@ -16,8 +29,8 @@ export async function fetchLatestArticles() {
     }
 
     const data = await response.json();
+    console.log("fetching", url);
     // renderArticles();
-    console.log(data);
     return data.response.results;
   } catch (error) {
     console.error("Error fetching articles:", error);
