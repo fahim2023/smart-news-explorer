55d808b7e486f4245316d42812ae0d35cf4dc82b
fahim2023 <fa733m4d4m@outlook.com>
2026-03-08 16:14:12 +0000

    add event listener to remove cards from bookmark upon clicking remove button

--- assets/js/bookmarkController.js
+++ assets/js/bookmarkController.js
@@ -4,4 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
   DefaultView.renderBookmarks(bookmarks);
+
+  document.addEventListener("click", (event) => {
+    const removeBtn = event.target.closest(".remove-bookmark");
+    if (!removeBtn) return;
+
+    const url = event.target.dataset.url;
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+    bookmarks = bookmarks.filter((bookmark) => bookmark.webUrl !== url);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    DefaultView.renderBookmarks(bookmarks);
+  });
 });
